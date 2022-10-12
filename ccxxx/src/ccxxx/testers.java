a18494c9a87da28fc7859dca23c8560fe26e7872
Lenovo <Lenovo@DESKTOP-FUHAHVF>
2022-10-12 22:12:48 +0530

    sd

---
+++ ccxxx/src/ccxxx/testers.java
@@ -0,0 +1,17 @@
+package ccxxx;
+
+public class testers {
+public int ff(int a, int b)
+{
+	int c;
+	c=a+b;
+	System.out.println("tetser"+c);
+	return c;
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+testers cc=new testers();
+cc.ff(23, 9)
+	}
+
+}
